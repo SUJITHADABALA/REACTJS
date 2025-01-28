914be72b0ce6c0fe6d1cfa894fa90c8c750d478c
SUJITHADABALA <sujithadabala@gmail.com>
2025-01-28 18:54:17 +0530

    Updated Heading component in React

--- App.js
+++ App.js
@@ -1,16 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "h1 tag"),
-    React.createElement("h2", {}, "h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag"),
-    React.createElement("h2", {}, "h2 tag"),
-  ]),
-]);
+// const heading = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "h1 tag"),
+//     React.createElement("h2", {}, "h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "h1 tag"),
+//     React.createElement("h2", {}, "h2 tag"),
+//   ]),
+// ]);
 
+//JSX
+
+// consts heading = (
+//   <h1 className="head" tabIndex="5">
+//     React using JSX
+//   </h1>
+// );
+
+const Title = function () {
+  return <h1>Hello</h1>;
+};
+// const Heading = function () {
+//   return (
+//     <div id="container">
+//       <Title />
+//       {Title()}
+//       <Title></Title>
+//       <h1 className="head">Namaste All</h1>
+//     </div>
+//   );
+// };
+
+const heading = (
+  <h1 className="head" tabIndex="5">
+    <Title></Title>
+    React using JSX
+  </h1>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Heading />);
 root.render(heading);
